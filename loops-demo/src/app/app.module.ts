c9bb2b503aa1a70c56dcde2bad15638f95f064df
Jyoti <jyothi@mobifyi.com>
2018-10-10 12:20:18 +0530

    header commit

--- loops-demo/src/app/app.module.ts
+++ loops-demo/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '@blox/material';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FeaturesComponent } from './features/features.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
--- loops-demo/src/app/header/header.component.ts
+++ loops-demo/src/app/header/header.component.ts
@@ -10,6 +10,17 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    function resizeHeaderOnScroll() {
+      const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+      shrinkOn = 200,
+      headerEl = document.getElementById('js-header');
+      if (distanceY > shrinkOn) {
+        headerEl.classList.add('smaller');
+      } else {
+        headerEl.classList.remove('smaller');
+      }
+    }
+    window.addEventListener('scroll', resizeHeaderOnScroll);
   }
 
 }
