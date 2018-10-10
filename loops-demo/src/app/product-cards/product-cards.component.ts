4aedb8bd5a5260824a3bd6d83274beb530d7125c
Jyoti <jyothi@mobifyi.com>
2018-10-10 13:45:59 +0530

    carosel css

--- loops-demo/src/app/product-cards/product-cards.component.ts
+++ loops-demo/src/app/product-cards/product-cards.component.ts
@@ -12,7 +12,38 @@ export class ProductCardsComponent implements OnInit {
     { img: 'http://placehold.it/350x150/333333' },
     { img: 'http://placehold.it/350x150/666666' }
   ];
-  slideConfig = { slidesToShow: 4, slidesToScroll: 4 };
+  slideConfig = {
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
 
   constructor() {}
 
