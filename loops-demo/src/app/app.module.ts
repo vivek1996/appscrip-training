2817b013b686025669fe709bbbe3e8d38b59fd0d
Jyoti <jyothi@mobifyi.com>
2018-10-10 10:37:42 +0530

    added slick caurosel

--- loops-demo/src/app/app.module.ts
+++ loops-demo/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterialModule } from '@blox/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
