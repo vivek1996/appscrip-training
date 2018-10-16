b36dd26a6c35a89789a4e1fecdc98119e8333165
Jyoti <jyothi@mobifyi.com>
2018-10-16 18:07:13 +0530

    removed ng-prime

--- boatzon-demo/src/app/app-routing.module.ts
+++ boatzon-demo/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren:
+      './local-busniness/local-busniness.module#LocalBusninessModule'
+  },
+  {
+    path: 'profile',
+    loadChildren: './profile-view/profile-view.module#ProfileViewModule'
+  },
+  {
+    path: 'business-profile',
+    loadChildren:
+      './business-profile/business-profile.module#BusinessProfileModule'
+  },
+  {
+    path: '**',
+    loadChildren:
+      './local-busniness/local-busniness.module#LocalBusninessModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
